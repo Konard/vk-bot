4e0d1d7e1f90ec5b734808e11e8eb41d51273bbb
Konstantin Dyachenko <drakonard@gmail.com>
2023-12-09 21:11:02 +0300

    Better friends loading.

--- birthdays.js
+++ birthdays.js
@@ -3,31 +3,43 @@ const token = require('fs').readFileSync('token', 'utf-8').trim();
 const vk = new VK({ token });
 
 async function congratulateFriendsWithBD() {
-    const { items } = await vk.api.friends.get({
-        fields: ['bdate']
-    });
+  let offset = 0;
+  const currentDate = new Date();
+  const currentDay = currentDate.getDate();
+  const currentMonth = currentDate.getMonth() + 1;
 
-    console.log(items.length);
+  while (true) {
+      if (offset >= 10000) {
+        break;
+      }
 
-    const currentDate = new Date();
-    const currentDay = currentDate.getDate();
-    const currentMonth = currentDate.getMonth() + 1;
-    
-    items.forEach(async (friend) => {
-        if (friend.id == 356592893){
-          console.log('friend', JSON.stringify(friend, null, 2));
-        }
-        if (friend.bdate) {
-            const [day, month] = friend.bdate.split('.');
-            if (day == currentDay && month == currentMonth) {
-                console.log('friend.id', friend.id)
-                // await vk.api.messages.send({
-                //     userId: friend.id,
-                //     message: `Happy Birthday, ${friend.first_name}! 🥳`
-                // });
-            }
-        }
-    });
+      const response = await vk.api.friends.get({
+          fields: ['bdate'],
+          count: 5000,
+          offset,
+      });
+
+      console.log('response.items.length', response.items.length);
+
+      if (response.items.length === 0) {
+          break;
+      }
+
+      for (const friend of response.items) {
+          if (friend.bdate) {
+              const [day, month] = friend.bdate.split('.');
+              if (day == currentDay && month == currentMonth) {
+                  console.log('friend.id', friend.id)
+                  // await vk.api.messages.send({
+                  //     userId: friend.id,
+                  //     message: `Happy Birthday, ${friend.first_name}! 🥳`
+                  // });
+              }
+          }
+      }
+
+      offset += 5000;
+  }
 }
 
 congratulateFriendsWithBD().catch(console.error);
