35a28734f6edecf7e9062e7736d0e91080630cd8
Konstantin Dyachenko <drakonard@gmail.com>
2023-12-09 03:38:46 +0300

    Added interval to reduce load on vk servers. Fixes #1

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ const fs = require('fs');
 const { VK } = require('vk-io');
 
 function randomInRange(min, max) {
-  return Math.random() * (max - min) + min;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 const messagesQueue = [];
-const intervalsToMessage = 5;
+let intervalsToMessage = 0;
+
+function increaseInterval() {
+  const randomNumber = randomInRange(2, 10);
+  console.log('increaseInterval', 'randomNumber', randomNumber);
+  intervalsToMessage += randomNumber;
+  console.log('increaseInterval', 'intervalsToMessage', intervalsToMessage);
+}
 
 fs.readFile('token', 'utf8' , (err, data) => {
   if (err) {
@@ -20,27 +29,27 @@ fs.readFile('token', 'utf8' , (err, data) => {
 
   vk.updates.on(['message_new'], (context) => {
     console.log('context.text', context.text);
+    increaseInterval();
     messagesQueue.push(context);
+    console.log('context', JSON.stringify(context, null, 2));
   });
 
   vk.updates.start().catch(console.error);
 
   const messagesHandlerInterval = setInterval(() => {
-    const message = messagesQueue.shift();
     if (intervalsToMessage > 0)
     {
+      console.log('intervalsToMessage', intervalsToMessage);
       intervalsToMessage--;
       return;
     }
+    const message = messagesQueue.shift();
     if (!message) {
       return;
     }
     if (message.text && message.text.toLowerCase() == 'hi') {
       message.send('Hello!');
+      increaseInterval();
     }
-    const randomNumber = randomInRange(2, 10);
-    console.log('randomNumber', randomNumber);
-    intervalsToMessage += randomNumber;
-    console.log('intervalsToMessage', intervalsToMessage);
   }, 1000);
 })
