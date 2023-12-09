a3e788f254b3be13963210ed22a36d034feb79a4
Konstantin Dyachenko <drakonard@gmail.com>
2023-12-09 03:24:54 +0300

    random interval is added

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const fs = require('fs');
 const { VK } = require('vk-io');
 
+function randomInRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
 const messagesQueue = [];
+const intervalsToMessage = 5;
 
 fs.readFile('token', 'utf8' , (err, data) => {
   if (err) {
@@ -22,11 +27,20 @@ fs.readFile('token', 'utf8' , (err, data) => {
 
   const messagesHandlerInterval = setInterval(() => {
     const message = messagesQueue.shift();
+    if (intervalsToMessage > 0)
+    {
+      intervalsToMessage--;
+      return;
+    }
     if (!message) {
       return;
     }
     if (message.text && message.text.toLowerCase() == 'hi') {
       message.send('Hello!');
     }
+    const randomNumber = randomInRange(2, 10);
+    console.log('randomNumber', randomNumber);
+    intervalsToMessage += randomNumber;
+    console.log('intervalsToMessage', intervalsToMessage);
   }, 1000);
 })
