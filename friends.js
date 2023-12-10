1c3c32e34d900ed28bf35629cda5b6a14f390dca
Konstantin Dyachenko <drakonard@gmail.com>
2023-12-10 04:55:10 +0300

    Make better logic for friends script

--- friends.js
+++ friends.js
@@ -2,89 +2,102 @@ const { VK } = require('vk-io');
 const token = require('fs').readFileSync('token', 'utf-8').trim();
 const vk = new VK({ token });
 
-const sourceCommunity = 54530371;
+const sourceCommunityId = 54530371;
 
 const sex = {
   male: 2
 };
 
-async function getOnlineFollowers(groupId) {
-    let response = await vk.api.groups.getMembers({ group_id: groupId, sort: "id_desc", fields: ["online", "can_send_friend_request", "can_write_private_message", "can_see_all_posts", "is_friend", "sex", "has_photo", "language", "city"] });
-    // let friends = await vk.api.friends.get();
-    let requests = await vk.api.friends.getRequests({ out: 1 });
-    
-    console.log('response.items.length', response.items.length);
-    console.log('response.items[0]', response.items[0]);
-    // console.log('friends.items.length', friends.items.length);
-    console.log('requests.items.length', requests.items.length);
+const requestsLimit = 10000; // Maximum number of requests you expect
+const requestsSegmentSize = 1000; // Number of requests fetched per segment
 
-    let onlineFollowers = [], friendIds = [], requestIds = [];
+async function fetchRequests(segment, offset) {
+  const req = await vk.api.friends.getRequests({ out: 1, count: segment, offset: offset });
+  return req || [];
+}
 
-    // friends.items.forEach(item => friendIds.push(item));
-    requests.items.forEach(item => requestIds.push(item));
+async function fetchAllRequests() {
+  let requestsIds = [];
+  for (let offset = 0; offset < requestsLimit; offset += requestsSegmentSize) {
+    const segment = await fetchRequests(requestsSegmentSize, offset);
+    requestsIds = requestsIds.concat(segment.items);
+    if (segment.items.length < requestsSegmentSize) {
+      // Early exit if we fetched less requests than requested: end of data.
+      break;
+    }
+  }
+  return requestsIds;
+}
 
-    // {
-    //   id: 154,
-    //   has_photo: 1,
-    //   is_friend: 0,
-    //   can_see_all_posts: 0,
-    //   can_write_private_message: 0,
-    //   can_send_friend_request: 0,
-    //   sex: 2,
-    //   online: 0,
-    //   first_name: 'Elisey',
-    //   last_name: 'Zamakhov',
-    //   can_access_closed: false,
-    //   is_closed: true
-    // }
+async function getOnlineFollowers(groupId, requestsIds) {
+  let response = await vk.api.groups.getMembers({ group_id: groupId, sort: "id_desc", fields: ["online", "can_send_friend_request", "can_write_private_message", "can_see_all_posts", "is_friend", "sex", "has_photo", "language", "city"] });
 
-    for(let follower of response.items){
-      if (!follower.online) {
-        continue;
-      }
-      if (follower.sex != sex.male) {
-        continue;
-      }
-      if (follower.is_friend) {
-        continue;
-      }
-      if (!follower.has_photo) {
-        continue;
-      }
-      if (!follower.can_see_all_posts) {
-        continue;
-      }
-      if (!follower.can_write_private_message) {
-        continue;
-      }
-      if (!follower.can_send_friend_request) {
-        continue;
-      }
+  console.log('response.items.length', response.items.length);
 
-      console.log('follower', follower);
+  let onlineFollowers = [], friendIds = [], requestIds = [];
 
-      // if(!friendIds.includes(follower) && !requestIds.includes(follower)){
-      if(!requestIds.includes(follower)) {
-          onlineFollowers.push(follower.id);
-      }
+  for (let follower of response.items) {
+    if (!follower.online) {
+      continue;
+    }
+    if (follower.sex != sex.male) {
+      continue;
+    }
+    if (follower.is_friend) {
+      continue;
+    }
+    if (!follower.has_photo) {
+      continue;
+    }
+    if (!follower.can_see_all_posts) {
+      continue;
+    }
+    if (!follower.can_write_private_message) {
+      continue;
+    }
+    if (!follower.can_send_friend_request) {
+      continue;
+    }
+    if (follower.is_closed) {
+      continue;
+    }
+    if (!follower.can_access_closed) {
+      continue;
     }
 
-    return onlineFollowers;
+    console.log('follower', follower);
+
+    if (!requestsIds.includes(follower.id)) {
+      onlineFollowers.push(follower.id);
+    }
+  }
+
+  return onlineFollowers;
 }
 
 async function sendFriendRequest(userId) {
-    return vk.api.friends.add({ user_id: userId });
+  return vk.api.friends.add({ user_id: userId });
 }
 
 async function main() {
-    let onlineFollowers = await getOnlineFollowers(sourceCommunity);
+  const requestsIds = await fetchAllRequests();
+  let onlineFollowersIds = await getOnlineFollowers(sourceCommunityId, requestsIds);
 
-    for(let user of onlineFollowers) {
-      console.log(`Friend request will be sent to: ${user}`);
-        sendFriendRequest(user)
-            .then(response => console.log(`Friend request sent to: ${user}`))
-            .catch(err => console.log(err));
+  if (onlineFollowersIds.length <= 0) {
+    console.log('No followers to add as friends found.');
+    return;
+  }
+
+  const messagesHandlerInterval = setInterval(() => {
+    const followerId = onlineFollowersIds.shift(); // dequeue follower
+    if (!followerId) {
+      return;
     }
+    console.log(`Friend request will be sent to: ${followerId}`);
+    sendFriendRequest(followerId)
+      .then(response => console.log(`Friend request sent to: ${followerId}`))
+      .catch(err => console.log(err));
+  }, 5000);
 }
 
 main();
