0dc0e61c84a672465b8164b012c83df56b6ea8c5
Konstantin Dyachenko <drakonard@gmail.com>
2023-12-17 16:24:39 +0000

    Update well being trigger test to use test.each instead of loops

--- tests/well-being-trigger.test.js
+++ tests/well-being-trigger.test.js
@@ -2,45 +2,41 @@ const { wellBeingTrigger } = require('../triggers/well-being');
 const { enqueueMessage } = require('../outgoing-messages');
 jest.mock('../outgoing-messages');
 
+const expectedOutgoingMessages = [
+  'Хорошо',
+  'Всё хорошо'
+];
+const expectedOutgoingMessagesRegex = new RegExp(expectedOutgoingMessages.join('|'), "i");
+
 describe('wellBeingTrigger', () => {
   beforeEach(() => {
     enqueueMessage.mockClear();
   });
-  it('matches well being question and gives expected response', () => {
-    const incommingMessages = [
-      'Как дела?',
-      'Как жизнь?',
-    ];
-    const outgoingMessages = [
-      'Хорошо',
-      'Всё хорошо'
-    ];
-    const outgoingMessagesRegex = new RegExp(outgoingMessages.join('|'), "i");
-    let c = 0;
-    for (const message of incommingMessages) {
-      const context = { request: { text: message } };
-      expect(wellBeingTrigger.condition(context)).toBe(true);
-      if (wellBeingTrigger.condition(context)) {
-        wellBeingTrigger.action(context);
-      }
-      expect(enqueueMessage).toHaveBeenCalled();
-      const callArg = enqueueMessage.mock.calls[c++][0];
-      expect(callArg).toEqual(expect.objectContaining(context));
-      expect(callArg.response.message).toMatch(outgoingMessagesRegex);
+
+  test.each([
+    ['Как дела?'],
+    ['Как жизнь?']
+  ])('matches well being question and gives expected response', (incomingMessage) => {
+    const context = { request: { text: incomingMessage } };
+    expect(wellBeingTrigger.condition(context)).toBe(true);
+    if (wellBeingTrigger.condition(context)) {
+      wellBeingTrigger.action(context);
     }
+    expect(enqueueMessage).toHaveBeenCalled();
+    const callArg = enqueueMessage.mock.calls[0][0];
+    expect(callArg).toEqual(expect.objectContaining(context));
+    expect(callArg.response.message).toMatch(expectedOutgoingMessagesRegex);
   });
-  it('does not match other questions', () => {
-    const incommingMessages = [
-      'Чем занимаешься?',
-      'Какая цель добавления в друзья?',
-    ];
-    for (const message of incommingMessages) {
-      const context = { request: { text: message } };
-      expect(wellBeingTrigger.condition(context)).toBe(false);
-      if (wellBeingTrigger.condition(context)) {
-        wellBeingTrigger.action(context);
-      }
-      expect(enqueueMessage).not.toHaveBeenCalled();
+
+  test.each([
+    ['Чем занимаешься?'], 
+    ['Какая цель добавления в друзья?']
+  ])('does not match other questions', (incomingMessage) => {
+    const context = { request: { text: incomingMessage } };
+    expect(wellBeingTrigger.condition(context)).toBe(false);
+    if (wellBeingTrigger.condition(context)) {
+      wellBeingTrigger.action(context);
     }
+    expect(enqueueMessage).not.toHaveBeenCalled();
   });
 });
