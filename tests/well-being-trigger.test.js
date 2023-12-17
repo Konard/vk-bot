19ec884d974f2cb7bc689190ba0d353a4e4e7fdb
Konstantin Dyachenko <drakonard@gmail.com>
2023-12-17 16:13:09 +0000

    Update well being trigger test to include multiple cases

--- tests/well-being-trigger.test.js
+++ tests/well-being-trigger.test.js
@@ -6,31 +6,41 @@ describe('wellBeingTrigger', () => {
   beforeEach(() => {
     enqueueMessage.mockClear();
   });
-
-  it('matches well being question and gives a response', () => {
-    const context = { request: { text: 'Как жизнь?' } };
-
-    expect(wellBeingTrigger.condition(context)).toBe(true);
-
-    if (wellBeingTrigger.condition(context)) {
-      wellBeingTrigger.action(context);
+  it('matches well being question and gives expected response', () => {
+    const incommingMessages = [
+      'Как дела?',
+      'Как жизнь?',
+    ];
+    const outgoingMessages = [
+      'Хорошо',
+      'Всё хорошо'
+    ];
+    const outgoingMessagesRegex = new RegExp(outgoingMessages.join('|'), "i");
+    let c = 0;
+    for (const message of incommingMessages) {
+      const context = { request: { text: message } };
+      expect(wellBeingTrigger.condition(context)).toBe(true);
+      if (wellBeingTrigger.condition(context)) {
+        wellBeingTrigger.action(context);
+      }
+      expect(enqueueMessage).toHaveBeenCalled();
+      const callArg = enqueueMessage.mock.calls[c++][0];
+      expect(callArg).toEqual(expect.objectContaining(context));
+      expect(callArg.response.message).toMatch(outgoingMessagesRegex);
     }
-
-    expect(enqueueMessage).toHaveBeenCalled();
-
-    const callArg = enqueueMessage.mock.calls[0][0];
-    expect(callArg).toEqual(expect.objectContaining(context));
-    expect(callArg.response.message).toMatch(/Хорошо|Всё хорошо/);
   });
-  it('does not match non well being question', () => {
-    const context = { request: { text: 'Чем занимаешься?' } };
-
-    expect(wellBeingTrigger.condition(context)).toBe(false);
-    
-    if (wellBeingTrigger.condition(context)) {
-      wellBeingTrigger.action(context);
+  it('does not match other questions', () => {
+    const incommingMessages = [
+      'Чем занимаешься?',
+      'Какая цель добавления в друзья?',
+    ];
+    for (const message of incommingMessages) {
+      const context = { request: { text: message } };
+      expect(wellBeingTrigger.condition(context)).toBe(false);
+      if (wellBeingTrigger.condition(context)) {
+        wellBeingTrigger.action(context);
+      }
+      expect(enqueueMessage).not.toHaveBeenCalled();
     }
-
-    expect(enqueueMessage).not.toHaveBeenCalled();
   });
 });
