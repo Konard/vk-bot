f1d54774c581de34aac52dd5f68c7228c02609eb
Konstantin Dyachenko <drakonard@gmail.com>
2023-12-17 18:42:59 +0000

    Better attachments logging.

--- triggers/attachments.js
+++ triggers/attachments.js
@@ -8,6 +8,25 @@ if (fs.existsSync(targetPath)) {
     receivedAttachments = JSON.parse(rawData);
 }
 
+function isEmptyArray(arr) {
+  arr = arr.filter(item => !(item === null || item === undefined || item === '')); 
+  return arr.length == 0;
+}
+
+function clean(obj) {
+  for (var propName in obj) { 
+    if (obj[propName] === null || obj[propName] === undefined || obj[propName] === '' ||
+       (Array.isArray(obj[propName]) && isEmptyArray(obj[propName]))  // check for empty arrays
+      ) {
+      delete obj[propName];
+    }
+    if(typeof obj[propName] === 'object'){
+      clean(obj[propName]); //recursive for nested objects
+    }
+  }
+  return obj;
+}
+
 const attachmentsTrigger = {
   condition: (context) => {
     return context?.request?.attachments?.length > 0;
@@ -15,9 +34,11 @@ const attachmentsTrigger = {
   action: (context) => {
     let newAttachments = false;
     for (const attachment of context.request.attachments) {
-      console.log('new', 'attachment', attachment);
+      console.log('attachment', 'before clean', attachment);
+      attachment = clean(attachment);
+      console.log('attachment', 'after clean', attachment);
       const id = attachment?.id;
-      if (id && !receivedAttachments[id]) { 
+      if (id && !receivedAttachments[id]) {
         receivedAttachments[id] = attachment;
         newAttachments = true;
       }
