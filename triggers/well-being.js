95d955a49b03803566889c3463f1ccc60e1b4246
Konstantin Dyachenko <drakonard@gmail.com>
2023-12-17 08:14:26 +0300

    Add draft for https://github.com/Konard/vk-bot/issues/13

---
+++ triggers/well-being.js
@@ -0,0 +1,29 @@
+const { getRandomElement } = require('./utils');
+const { enqueueMessage } = require('../outgoing-messages');
+
+// Как дела?
+// Как жизнь?
+const wellBeingQuestionRegex = /^\s*Как (дела|жизнь)[\s?)\\]*$/ui;
+
+const wellBeingAnswers = [
+  "Хорошо",
+  "Всё хорошо",
+];
+
+const wellBeingTrigger = {
+  condition: (context) => {
+    return wellBeingQuestionRegex.test(context.request.text);
+  },
+  action: (context) => {
+    enqueueMessage({
+      ...context,
+      response: {
+        message: getRandomElement(wellBeingAnswers)
+      }
+    });
+  }
+};
+
+module.exports = {
+  wellBeingTrigger
+};
