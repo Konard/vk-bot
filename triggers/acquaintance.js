6f656e7cf5a8574015d764e4cae48a52718b2541
Konstantin Dyachenko <drakonard@gmail.com>
2023-12-24 23:06:12 +0300

    Bug fix

--- triggers/acquaintance.js
+++ triggers/acquaintance.js
@@ -1,5 +1,6 @@
 const { getRandomElement } = require('../utils');
 const { enqueueMessage } = require('../outgoing-messages');
+const { DateTime } = require('luxon');
 
 const acquaintedRegex = /^\s*(мы\s*)?знакомы(\s*с\s*(тобой|вами))?[\s?)\\]*$/ui;
 
