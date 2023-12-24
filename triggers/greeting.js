da436649faa66dbc76902912bc8467c50d8ccefd
Konstantin Dyachenko <drakonard@gmail.com>
2023-12-24 22:51:48 +0300

    Update logic for https://github.com/Konard/vk-bot/issues/4

--- triggers/greeting.js
+++ triggers/greeting.js
@@ -65,9 +65,9 @@ const greetingTrigger = {
     console.log('context?.state?.triggers?.[greetingTrigger.name]', context?.state?.triggers?.[greetingTrigger.name]);
     console.log('context?.state?.triggers?.[greetingTrigger.name]?.lastTriggered', context?.state?.triggers?.[greetingTrigger.name]?.lastTriggered);
     const lastTriggered = context?.state?.triggers?.[greetingTrigger.name]?.lastTriggered;
-    const lastTriggeredDiff = lastTriggered ? now.diff(lastTriggered, 'days').days : 0;
+    const lastTriggeredDiff = lastTriggered ? now.diff(lastTriggered, 'days').days : Number.MAX_SAFE_INTEGER;
     console.log('lastTriggeredDiff', lastTriggeredDiff);
-    return lastTriggeredDiff <= 1
+    return lastTriggeredDiff >= 1
         || greetingRegex.test(context.request.text)
         || hasSticker(context.request, incomingGreetingStickersIds);
   },
