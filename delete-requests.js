4a652b44f25ba5c3e318e085f5c0b06da256ba10
Konstantin Dyachenko <drakonard@gmail.com>
2023-12-24 09:18:27 +0300

    Add delete friend requests script

---
+++ delete-requests.js
@@ -0,0 +1,18 @@
+const { sleep } = require('./utils');
+const token = require('fs').readFileSync('token', 'utf-8').trim();
+const vk = new VK({ token });
+
+async function deleteFriendRequests() {
+  try {
+      const requests = await vk.api.friends.getRequests({ count: 23, out: 1 });
+      for (let i = 0; i < requests.items.length; i++) {
+          await vk.api.friends.delete({ user_id: requests.items[i] });
+          await sleep(3000);
+      }
+      return requests;
+  } catch (error) {
+      console.error(error);
+  }
+}
+
+deleteFriendRequests();
