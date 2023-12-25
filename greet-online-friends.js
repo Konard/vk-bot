b6ea7ba91cf92001a87e5d10217968229a8097b3
Konstantin Dyachenko <drakonard@gmail.com>
2023-12-25 08:44:36 +0300

    Add greet online friends script draft

---
+++ greet-online-friends.js
@@ -0,0 +1,60 @@
+const { VK } = require('vk-io');
+const { getRandomElement } = require('./utils');
+const { randomInRange, handleOutgoingMessage, enqueueMessage } = require('./outgoing-messages');
+const token = require('fs').readFileSync('token', 'utf-8').trim();
+const vk = new VK({ token });
+
+async function greetOnlineFriends() {
+  let offset = 0;
+  // const currentDate = new Date();
+  // const currentDay = currentDate.getDate();
+  // const currentMonth = currentDate.getMonth() + 1;
+
+  while (true) {
+    if (offset >= 10000) {
+      break;
+    }
+
+    const response = await vk.api.friends.get({
+      fields: ['bdate', 'online'],
+      count: 5000,
+      offset,
+    });
+
+    console.log('response.items.length', response.items.length);
+
+    if (response.items.length === 0) {
+      break;
+    }
+
+    for (const friend of response.items) {
+      console.log('friend.online', friend.online)
+      // if (friend.bdate) {
+      //   const [day, month] = friend.bdate.split('.');
+      //   if (day == currentDay && month == currentMonth) {
+      //     console.log('friend.id', friend.id);
+      //     enqueueMessage({
+      //       vk,
+      //       response: {
+      //         user_id: friend.id,
+      //         sticker_id: getRandomElement(birthdayStickerIds),
+      //       }
+      //     });
+      //     enqueueMessage({
+      //       vk,
+      //       response: {
+      //         user_id: friend.id,
+      //         message: getRandomElement(birthdayCongratulations)
+      //       }
+      //     });
+      //   }
+      // }
+    }
+
+    offset += 5000;
+  }
+}
+
+greetOnlineFriends().catch(console.error);
+
+const messagesHandlerInterval = setInterval(handleOutgoingMessage, 1000);
