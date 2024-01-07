2af275079db937b5dc9f940a5e03206f027ed228
Konstantin Dyachenko <drakonard@gmail.com>
2024-01-07 10:13:10 +0300

    Add argument that will allow configure the number of friends requests to be deleted

--- delete-requests.js
+++ delete-requests.js
@@ -3,9 +3,11 @@ const { VK } = require('vk-io');
 const token = require('fs').readFileSync('token', 'utf-8').trim();
 const vk = new VK({ token });
 
+const deletedFriendsRequestsCount = Number(process.argv[2]) || 0;
+
 async function deleteFriendRequests() {
   try {
-    const requests = await vk.api.friends.getRequests({ count: 23, out: 1, need_viewed: 1 });
+    const requests = await vk.api.friends.getRequests({ count: deletedFriendsRequestsCount, out: 1, need_viewed: 1 });
     for (let i = 0; i < requests.items.length; i++) {
       await vk.api.friends.delete({ user_id: requests.items[i] });
       console.log('deleted friend request:', requests.items[i])
