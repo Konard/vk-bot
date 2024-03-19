c61c01980483c2e5267f9e45d3f73d835d6cf26d
Konstantin Dyachenko <drakonard@gmail.com>
2024-03-19 23:19:02 +0300

    Add more debug output for friend greeting process

--- greet-online-friends.js
+++ greet-online-friends.js
@@ -23,30 +23,37 @@ async function greetOnlineFriends() {
     }
 
     for (const friend of response.items) {
-      if (!friend.online || !friend.can_access_closed || friend.is_closed) {
+      if (!friend.can_access_closed || friend.is_closed) {
+        console.log(`Skipping friend ${friend.id}:
+friend.can_access_closed == ${friend.can_access_closed};
+friend.is_closed == ${friend.is_closed}.`)
         continue;
       }
       console.log(friend);
 
+      console.log(`Loading conversations for ${friend.id} friend.`)
       const response = await vk.api.messages.getConversationsById({
         peer_ids: [friend.id],
         count: 1
       });
-      await sleep(30000);
+      await sleep(20000);
 
       const conversation = response.items[0];
 
       if (conversation.last_message_id != 0 || conversation.last_conversation_message_id != 0)
       {
+        console.log(`Skipping friend ${friend.id} because conversation history is not empty.`);
         continue;
       }
 
-      greetingTrigger.action({
+      await greetingTrigger.action({
         vk,
         response: {
           user_id: friend.id,
         }
       });
+
+      console.log(`Greeting for friend ${friend.id} is added to queue.`);
 
       greetedFriends++;
       console.log('greetedFriends:', greetedFriends);
