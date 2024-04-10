4f2e493717862b5de16b6de1fc8a108124d3fef1
Konstantin Dyachenko <drakonard@gmail.com>
2024-04-10 18:35:22 +0300

    Make a better reaction to cancelled friendships

--- triggers/react-to-cancelled-friendships.js
+++ triggers/react-to-cancelled-friendships.js
@@ -6,6 +6,34 @@ const questions = [
   'Почему не хочешь больше дружить?'
 ];
 
+const targetPath = 'friends-conversations.json';
+
+let friendsConversations = {};
+if (fs.existsSync(targetPath)) {
+  friendsConversations = JSON.parse(fs.readFileSync(targetPath));
+  console.log('Object.keys(friendsConversations).length', Object.keys(friendsConversations).length)
+}
+
+function clean(obj) {
+  for (var propName in obj) { 
+    if (obj[propName] === null || obj[propName] === undefined || obj[propName]?.length === 0) {
+      delete obj[propName];
+    }
+    // if(typeof obj[propName] === 'object'){
+    //   clean(obj[propName]); //recursive for nested objects
+    // }
+  }
+  return obj;
+}
+
+function eraseMetadata(obj) {
+  return JSON.parse(JSON.stringify(obj));
+}
+
+function saveToFile() {
+  fs.writeFileSync(targetPath, JSON.stringify(friendsConversations, null, 2));
+}
+
 async function reactToCancelledFriendships(context) {
   try {
     const count = context?.options?.maxRequests;
@@ -13,6 +41,7 @@ async function reactToCancelledFriendships(context) {
       return;
     }
     const requests = await context.vk.api.friends.getRequests({ count, out: 1, need_viewed: 1 });
+    await sleep(3000);
     if (requests.items.length <= 0) {
       console.log('No cancelled friendships to react to.');
       return requests;
@@ -20,34 +49,51 @@ async function reactToCancelledFriendships(context) {
     for (let i = 0; i < requests.items.length; i++) {
       const friendId = requests.items[i];
       try {
-        if (context?.states?.[friendId]?.reactedToCancelledFriendRequest) {
-          console.log('Skipping reaction to cancelled friendship for friend', friendId);
-          continue;
+        let conversation;
+        if (friendsConversations[friendId]) {
+          conversation = friendsConversations[friendId];
+        } else {
+          conversation = await context.vk.api.messages.getConversationsById({
+            peer_ids: [friendId],
+            count: 1
+          }).items[0];
+          await sleep(20000);
         }
 
-        await greetingTrigger.action({
-          vk: context.vk,
-          response: {
-            user_id: friendId,
+        if (!conversation.can_write.allowed) {
+          await context.vk.api.account.ban({
+            owner_id: friendId
+          });
+          console.log(`Friend ${friendId} is blocked because it is not allowed to send message to this friend, and friendship is cancelled.`);
+        } else {
+          if (context?.states?.[friendId]?.reactedToCancelledFriendRequest) {
+            console.log('Skipping reaction to cancelled friendship for friend', friendId);
+            continue;
           }
-        });
-        enqueueMessage({
-          vk: context.vk,
-          response: {
-            user_id: friendId,
-            message: getRandomElement(questions),
-          }
-        });
-
-        const states = context.states ??= {};
-        const friendState = states[friendId] ??= {};
-        friendState.reactedToCancelledFriendRequest = true;
-
-        console.log('Reacted to cancelled friendship for friend', friendId);
+  
+          await greetingTrigger.action({
+            vk: context.vk,
+            response: {
+              user_id: friendId,
+            }
+          });
+          enqueueMessage({
+            vk: context.vk,
+            response: {
+              user_id: friendId,
+              message: getRandomElement(questions),
+            }
+          });
+  
+          const states = context.states ??= {};
+          const friendState = states[friendId] ??= {};
+          friendState.reactedToCancelledFriendRequest = true;
+  
+          console.log('Reacted to cancelled friendship for friend', friendId);
+        }
       } catch (error) {
         console.error(`Could not react to cancelled friendship for friend ${friendId}: ${error}`);
       }
-      await sleep(3000);
     }
     return requests;
   } catch (error) {
